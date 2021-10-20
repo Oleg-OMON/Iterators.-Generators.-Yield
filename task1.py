f5f9d7b226b4b828bc58ed8c402453d09712ee4c
Oleg Zasedatelev <ol.zasedatelew@yandex.ru>
2021-10-20 14:45:10 +0300

    file added

---
+++ task1.py
@@ -0,0 +1,42 @@
+import hashlib
+import json
+
+wiki_url = 'https://ru.wikipedia.org/wiki/'
+
+
+class MyIterator:
+
+    def __init__(self, path):
+        with open('countries.json', 'r') as file:
+            country_list = json.load(file)
+            for country in country_list:
+                name = country['name']['common']
+                self.iter = iter(name)
+
+    def url_country(self, country_name: str):
+        country_name = country_name.replace(' ', '_')
+        country_url = f'{wiki_url}{country_name}'
+        return country_url
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        country_name = next(self.iter)
+        result = f'{country_name} - {self.url_country(country_name)}'
+        return result
+
+
+def hash(path: str):
+    with open(path) as file:
+        for line in file:
+            yield hashlib.md5(line.encode()).hexdigest()
+
+
+if __name__ == '__main__':
+    with open('counties_link.txt', 'w') as country_names_file:
+        for country_link in MyIterator('countries.json'):
+            country_names_file.write(f'{country_link}\n')
+
+    for hash_str in hash('counties_link.txt'):
+        print(hash_str)
